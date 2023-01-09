65727e5c80133cb0941359c66c1d493fa6ca3255
adnan614 <adnanfaruque23@gmail.com>
2023-01-09 09:08:18 +0600

    up

---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <div>
+            <input type="text" />
+            <input type="text" />
+            <button>
+                Login
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Login
